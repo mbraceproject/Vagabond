0dcbe55cab920d242c4c7666b3cd17c478ad8de8
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-02-24 20:59:37 +0200

    add Mono.Reflection

--- src/DistribFsi.Shell/Script1.fsx
+++ src/DistribFsi.Shell/Script1.fsx
@@ -1,56 +1,83 @@
 ﻿#r "bin/Debug/DistribFsi.Settings.dll"
 #r "../../samples/ThunkServer/bin/Debug/ThunkServer.exe"
+#r "bin/Debug/Mono.Cecil.dll"
+#r "../../lib/Mono.Reflection.dll"
+
 
 open System
 open System.Reflection
 
-open Nessos.DistribFsi.Sample
+open Mono.Reflection
+open Mono.Cecil
 
 let ass = Assembly.GetExecutingAssembly()
 
-let lsType (t : Type) =
-    let modl = t.FullName.Split([|'.';'+'|]).[0]
-    let types = ass.GetTypes() |> Array.filter (fun t -> t.Name.Contains modl)
-    types |> Array.collect (fun t -> t.GetFields(BindingFlags.Static ||| BindingFlags.NonPublic))
-    
-    
-type Marker = class end
 
-let f =
-    let r = System.Random()
-    let x = r.Next()
-    let y = let x = r.Next() in x + 1
-    fun () -> x + y
-
-let f0 = Func f
-
-f ()
-
-
-type Marker2 = class end
-let foo =
-    let r = System.Random()
-    let v1 = r.Next(0,1)
-    let v2 = r.Next(0,2)
+type CompiledAssemblyState =
     {
-        new System.IDisposable with
-            member __.Dispose() = printfn "%d" <| v1 + v2
-        
+        CompiledAssemblyCount : int
+
+        FsiDynamicAssembly : Assembly
+        TypeIndex : Map<string, Assembly>
     }
 
-let _ = distribFsi.RequestCompilation()
-
-let flds = lsType typeof<Marker2>
-
-
-let client = initThunkServer()
-
-client.EvaluateThunk f0
+module Map =
+    let addMany (m : Map<'K,'V>) (kvs : ('K * 'V) seq) =
+        Seq.fold (fun (m : Map<_,_>) (k,v) -> m.Add(k,v)) m kvs
 
 
+let getAssemblyDiff (state : CompiledAssemblyState) =
+    let snapshot = Mono.Reflection.AssemblySaver.Read(state.FsiDynamicAssembly)
+    let mainModule = snapshot.MainModule
+    let types = mainModule.Types
+    let compiled, newTypes = 
+        mainModule.Types
+        |> Seq.toList
+        |> List.partition (fun td -> state.TypeIndex.ContainsKey td.FullName)
+
+    // TODO: resolve current interaction name
+
+    for t in compiled do types.Remove t |> ignore
+
+    let n = state.CompiledAssemblyCount + 1
+    let target = sprintf "C:/mbrace/%d.dll" n
+
+    let name = mainModule.Assembly.Name.Name <- sprintf "FSI_%03d" n
+
+    do snapshot.Write(target)
+
+    let assembly = Assembly.ReflectionOnlyLoadFrom(target)
+
+    let typeIndex = newTypes |> Seq.map (fun t -> t.FullName, assembly) |> Map.addMany state.TypeIndex
+
+    {
+        CompiledAssemblyCount = n
+        FsiDynamicAssembly = state.FsiDynamicAssembly
+        TypeIndex = typeIndex
+    }
+
+let state = { CompiledAssemblyCount = 0 ; FsiDynamicAssembly = ass ; TypeIndex = Map.empty }
+let state = getAssemblyDiff state
+
+type Foo = Bar of int
+let x = Bar 42
 
 
+///
 
-let _ = distribFsi.RequestCompilation()
+type Foo = Bar
 
+let rec collect (tds : seq<TypeDefinition>) =
+    seq {
+        for t in tds do
+            yield t
+            yield! collect t.NestedTypes
+    }
 
+let snapshot = AssemblySaver.Read(ass)
+
+let ts = snapshot.MainModule.Types |> collect |> Seq.toList
+
+let t = ts.[15]
+
+t.Fields

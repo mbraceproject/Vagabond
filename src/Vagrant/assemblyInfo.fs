09700a47e6d8208235aa56822a16c7be14661f16
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-07-04 16:47:53 +0300

    version

--- src/Vagrant/assemblyInfo.fs
+++ src/Vagrant/assemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.1.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.5")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.5"
+    let [<Literal>] Version = "0.2.0"

9fc90e1aebb34568607f2d07c0bb5342beb2350d
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-12-16 20:09:16 +0200

    bump versions

--- src/Vagabond/assemblyInfo.fs
+++ src/Vagabond/assemblyInfo.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.12.4")>]
-[<assembly: AssemblyFileVersionAttribute("0.12.4")>]
+[<assembly: AssemblyVersionAttribute("0.12.5")>]
+[<assembly: AssemblyFileVersionAttribute("0.12.5")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.12.4"
+    let [<Literal>] Version = "0.12.5"

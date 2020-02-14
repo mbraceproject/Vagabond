676ee4e9e4b1bd92c95504309fa1b7feacdffd1f
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-02-14 16:57:56 +0000

    fix build.cmd

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@
 dotnet tool restore
 
 REM Use paket and not fake for restoring packages
-REM c.f. https://github.com/fsharp/FAKE/issues/2181                                                                                                                                                                dotnet paket restore                                                                                                                                                                                               export PAKET_SKIP_RESTORE_TARGETS=true 
+REM c.f. https://github.com/fsharp/FAKE/issues/2181
 dotnet paket restore
-export PAKET_SKIP_RESTORE_TARGETS=true
+set PAKET_SKIP_RESTORE_TARGETS true
 
 dotnet fake run build.fsx %*
